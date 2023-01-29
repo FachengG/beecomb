d595740c9605f3de005b60e05e3a2b1a320df2fc
FG <43148888+FachengG@users.noreply.github.com>
2023-01-29 13:49:56 -0500

    draft

--- queen.py
+++ queen.py
@@ -29,7 +29,7 @@ class Queen():
         activate_bees = []
 
         for task_uuid in self.activate_bees:
-            if not Task_manager_db().check_is_task_done(task_uuid):
+            if not db().check_is_task_done(task_uuid):
                 activate_bees.append(task_uuid)
 
         self.activate_bees = activate_bees
--- utils/initial.py
+++ utils/initial.py
@@ -1,5 +1,5 @@
 from db_access import db
 
 # initial database
-initial_db = db_access.Task_manager_db()
+initial_db = db()
 initial_db.create_tasks_table()
